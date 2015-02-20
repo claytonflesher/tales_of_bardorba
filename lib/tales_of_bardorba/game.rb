95de1f5f3a5056e72b90b03bbfb347c91934538b
Clayton Flesher <claytonflesher@gmail.com>
2015-02-19 19:44:29 -0600

    expanded exploration to allow choice to stay in world after combat. Added resting in town to restore hp.

--- lib/tales_of_bardorba/game.rb
+++ lib/tales_of_bardorba/game.rb
@@ -7,11 +7,12 @@ require_relative "saved_game"
 module TalesOfBardorba
   class Game
     def initialize(player)
-      @player = player
-      @enemy_chooser = EnemyChooser.new
+      @player         = player
+      @enemy_chooser  = EnemyChooser.new
+      @in_village     = true
     end
 
-    attr_reader :player, :enemy_chooser
+    attr_reader :player, :enemy_chooser, :in_village
 
     def play
       until player.dead?
@@ -20,11 +21,22 @@ module TalesOfBardorba
     end
 
     def make_a_choice
+      if in_village?
+        village_action_choice
+      else
+        world_action_choice
+      end
+    end
+
+    def village_action_choice
+      @in_village = true
       puts "You find yourself in a rustic village in the middle of a strange but sexy world."
-      response = Input.new("What would you like to do?\n\n[E]xplore the world outside the village.\n[S]ave your game.\n[Q]uit with your tail between your legs.\n?", %w[E S Q]).get_char
+      response = Input.new("What would you like to do?\n\n[E]xplore the world outside the village.\n[R]est\n[S]ave your game.\n[Q]uit with your tail between your legs.\n?", %w[E R S Q]).get_char
       case response
       when "E"
         explore_world
+      when "R"
+        heal_player
       when "S"
         save
       when "Q"
@@ -33,10 +45,37 @@ module TalesOfBardorba
     end
 
     def explore_world
+      @in_village = false
       puts "As you step out into the world, you have a newfound sense of purpose and vim."
+      determine_world_event
+    end
+
+    def determine_world_event
+      #Add random selection between enemy combat and npc_encounter here. Heavily favor combat.
+      #if random outcome
+      # npc_encounter
+      #else
       enemy = enemy_chooser.choose
       puts "Suddenly, you encounter #{enemy.name}!"
       Combat.new(player, enemy).resolve
+      #end
+      make_a_choice
+    end
+
+    def world_action_choice
+      input = Input.new("That over, what would you like to do now?\n[R]eturn to the village.\n[C]ontinue exploring the world.", %w[R C]).get_char
+      case input
+      when "R"
+        @in_village = true
+        make_a_choice
+      when "C"
+        determine_world_event
+      end
+    end
+
+    def heal_player
+      player.hp = 100
+      puts "After a short period of time, you feel reinvigorated. You now have #{player.hp} HP."
     end
 
     def save
@@ -57,6 +96,12 @@ module TalesOfBardorba
       #FIXME
     end
 
+    def npc_encounter
+      #FIXME
+    end
+
+    def in_village?
+      in_village
+    end
   end
 end
-

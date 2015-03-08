79ac02eca630aa6d18fc5e4e44faf36289a5d4ab
James Edward Gray II <james@graysoftinc.com>
2015-03-08 10:59:45 -0500

    Extracting object saving and loading code and using it to load Job.

--- lib/tales_of_bardorba/job.rb
+++ lib/tales_of_bardorba/job.rb
@@ -1,3 +1,5 @@
+require_relative "serializer"
+
 module TalesOfBardorba
   class Job
     FILENAME = File.join(__dir__, "../../data/jobstats.txt")
@@ -16,37 +18,8 @@ module TalesOfBardorba
     attr_reader :job
     attr_accessor :hpmax, :hit, :defense, :magic, :feats, :hp, :stats
 
-    def load_file
-      File.open(FILENAME, "r") do |f|
-        while (line = f.gets) 
-          if line.include?(job)
-            6.times do
-              data = f.gets.strip.split("=")
-              format_stats(data)
-            end
-          end
-        end
-      end
-    end
-
-    def format_stats(data)
-      if data[1] =~ /[0-9]$/
-        @stats[data[0]] = data[1].to_i
-      elsif
-        data[1] =~ /(true|false)$/
-        @stats[data[0]] = data[1] == "true"
-      else
-        @stats[data[0]] = data[1]
-      end
-    end
-
-    def assign_stats
-      @hpmax    = @stats["hpmax"]
-      @hit      = @stats["hit"]
-      @defense  = @stats["defense"]
-      @magic    = @stats["magic"]
-      @feats    = @stats["feats"]
-      @hp       = @stats["hp"]
+    def load
+      Serializer.new(self).load(FILENAME, job)
     end
   end
 
@@ -54,4 +27,3 @@ module TalesOfBardorba
     @stats = stats
   end
 end
-

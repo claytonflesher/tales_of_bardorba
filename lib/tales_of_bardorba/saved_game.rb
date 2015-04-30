18c1e5665d6b822cc8c227c138b46e525a03efe3
Calvyn82 <claytonflesher@gmail.com>
2015-04-30 18:45:20 -0500

    Went with YAML. saved_game data file is easier to read

--- lib/tales_of_bardorba/saved_game.rb
+++ lib/tales_of_bardorba/saved_game.rb
@@ -1,15 +1,16 @@
 require_relative "player"
+require "yaml"
 
 module TalesOfBardorba
   class SavedGame
     FILENAME = File.join(__dir__, "../../data/saved_game.txt")
 
     def create(player)
-      File.open(FILENAME, "w") { |f| f.write Marshal.dump(player) }
+      File.open(FILENAME, "w") { |f| f.write YAML.dump(player) }
     end
 
     def load
-      player = Marshal.load(File.read(FILENAME))
+      player = YAML.load(File.read(FILENAME))
       player.reset_stats
       player
     end
